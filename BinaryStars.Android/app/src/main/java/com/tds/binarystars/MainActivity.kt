123f29bc6763f39855aeab233872b8f9eb645603
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2026-01-22 13:51:18 -0600

    All devices list

--- BinaryStars.Android/app/src/main/java/com/tds/binarystars/MainActivity.kt
+++ BinaryStars.Android/app/src/main/java/com/tds/binarystars/MainActivity.kt
@@ -46,11 +46,55 @@ class MainActivity : AppCompatActivity() {
                 isSynced = true,
                 wifiUploadSpeed = "50 Mbps",
                 wifiDownloadSpeed = "20 Mbps"
+            ),
+            Device(
+                id = "4",
+                name = "Samsung Tablet",
+                type = DeviceType.ANDROID,
+                ipAddress = "192.168.1.25",
+                batteryLevel = 45,
+                isConnected = false,
+                isSynced = false,
+                wifiUploadSpeed = "-",
+                wifiDownloadSpeed = "-"
+            ),
+             Device(
+                id = "5",
+                name = "Arch Linux Laptop",
+                type = DeviceType.LINUX,
+                ipAddress = "192.168.1.101",
+                batteryLevel = 60,
+                isConnected = false,
+                isSynced = false,
+                wifiUploadSpeed = "-",
+                wifiDownloadSpeed = "-"
+            ),
+            Device(
+                id = "6",
+                name = "OnePlus 9",
+                type = DeviceType.ANDROID,
+                ipAddress = "192.168.1.18",
+                batteryLevel = 92,
+                isConnected = true,
+                isSynced = true,
+                wifiUploadSpeed = "10 Kbps",
+                wifiDownloadSpeed = "500 Kbps"
             )
         )
 
+        val connectedDevices = devices.filter { it.isConnected }
+        val availableDevices = devices // "Available" list has connected true/false as per request (all devices)
+        
+        // Update header subtitle
+        val tvHeaderSubtitle = findViewById<android.widget.TextView>(R.id.tvHeaderSubtitle)
+        tvHeaderSubtitle.text = "${connectedDevices.size} devices connected"
+
         val rvDevices = findViewById<RecyclerView>(R.id.rvDevices)
         rvDevices.layoutManager = LinearLayoutManager(this)
-        rvDevices.adapter = DevicesAdapter(devices)
+        rvDevices.adapter = DevicesAdapter(connectedDevices)
+        
+        val rvAvailableDevices = findViewById<RecyclerView>(R.id.rvAvailableDevices)
+        rvAvailableDevices.layoutManager = LinearLayoutManager(this)
+        rvAvailableDevices.adapter = DevicesAdapter(availableDevices)
     }
 }
