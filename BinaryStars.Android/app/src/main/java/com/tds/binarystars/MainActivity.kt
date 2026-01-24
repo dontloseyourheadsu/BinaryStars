210d270153d3694b464c8120dc933b3b8c0de274
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2026-01-23 20:43:19 -0600

    Main activity navigation with new icons

--- BinaryStars.Android/app/src/main/java/com/tds/binarystars/MainActivity.kt
+++ BinaryStars.Android/app/src/main/java/com/tds/binarystars/MainActivity.kt
@@ -2,99 +2,44 @@ package com.tds.binarystars
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.tds.binarystars.adapter.DevicesAdapter
-import com.tds.binarystars.model.Device
-import com.tds.binarystars.model.DeviceType
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.tds.binarystars.fragments.DevicesFragment
+import com.tds.binarystars.fragments.FilesFragment
+import com.tds.binarystars.fragments.MapFragment
+import com.tds.binarystars.fragments.NotesFragment
+import com.tds.binarystars.fragments.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val devices = listOf(
-            Device(
-                id = "1",
-                name = "Ubuntu Workstation",
-                type = DeviceType.LINUX,
-                ipAddress = "192.168.1.10",
-                batteryLevel = 100,
-                isConnected = true,
-                isSynced = true,
-                wifiUploadSpeed = "12 Kbps",
-                wifiDownloadSpeed = "4.2 Mbps"
-            ),
-            Device(
-                id = "2",
-                name = "Pixel 7 Pro",
-                type = DeviceType.ANDROID,
-                ipAddress = "192.168.1.15",
-                batteryLevel = 87,
-                isConnected = true,
-                isSynced = true,
-                wifiUploadSpeed = "0 Kbps",
-                wifiDownloadSpeed = "2 Kbps"
-            ),
-            Device(
-                id = "3",
-                name = "Linux Server",
-                type = DeviceType.LINUX,
-                ipAddress = "192.168.1.200",
-                batteryLevel = 100,
-                isConnected = true,
-                isSynced = true,
-                wifiUploadSpeed = "50 Mbps",
-                wifiDownloadSpeed = "20 Mbps"
-            ),
-            Device(
-                id = "4",
-                name = "Samsung Tablet",
-                type = DeviceType.ANDROID,
-                ipAddress = "192.168.1.25",
-                batteryLevel = 45,
-                isConnected = false,
-                isSynced = false,
-                wifiUploadSpeed = "-",
-                wifiDownloadSpeed = "-"
-            ),
-             Device(
-                id = "5",
-                name = "Arch Linux Laptop",
-                type = DeviceType.LINUX,
-                ipAddress = "192.168.1.101",
-                batteryLevel = 60,
-                isConnected = false,
-                isSynced = false,
-                wifiUploadSpeed = "-",
-                wifiDownloadSpeed = "-"
-            ),
-            Device(
-                id = "6",
-                name = "OnePlus 9",
-                type = DeviceType.ANDROID,
-                ipAddress = "192.168.1.18",
-                batteryLevel = 92,
-                isConnected = true,
-                isSynced = true,
-                wifiUploadSpeed = "10 Kbps",
-                wifiDownloadSpeed = "500 Kbps"
-            )
-        )
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
 
-        val connectedDevices = devices.filter { it.isConnected }
-        val availableDevices = devices // "Available" list has connected true/false as per request (all devices)
-        
-        // Update header subtitle
-        val tvHeaderSubtitle = findViewById<android.widget.TextView>(R.id.tvHeaderSubtitle)
-        tvHeaderSubtitle.text = "${connectedDevices.size} devices connected"
+        // Load default fragment
+        if (savedInstanceState == null) {
+            loadFragment(DevicesFragment())
+        }
 
-        val rvDevices = findViewById<RecyclerView>(R.id.rvDevices)
-        rvDevices.layoutManager = LinearLayoutManager(this)
-        rvDevices.adapter = DevicesAdapter(connectedDevices)
-        
-        val rvAvailableDevices = findViewById<RecyclerView>(R.id.rvAvailableDevices)
-        rvAvailableDevices.layoutManager = LinearLayoutManager(this)
-        rvAvailableDevices.adapter = DevicesAdapter(availableDevices)
+        bottomNav.setOnItemSelectedListener { item ->
+            val fragment: Fragment = when (item.itemId) {
+                R.id.nav_devices -> DevicesFragment()
+                R.id.nav_files -> FilesFragment()
+                R.id.nav_notes -> NotesFragment()
+                R.id.nav_map -> MapFragment()
+                R.id.nav_settings -> SettingsFragment()
+                else -> DevicesFragment()
+            }
+            loadFragment(fragment)
+            true
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
