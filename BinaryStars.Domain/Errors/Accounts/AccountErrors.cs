ee738cec885e0f8e8f1fe2a061ccf5dfd55a2713
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2026-01-24 21:46:47 -0600

    Errors

---
+++ BinaryStars.Domain/Errors/Accounts/AccountErrors.cs
@@ -0,0 +1,7 @@
+namespace BinaryStars.Domain.Errors.Accounts;
+
+public static class AccountErrors
+{
+    public const string UserCannotBeDefault = "User cannot be default";
+    public const string DevicesCannotBeNull = "Devices list cannot be null";
+}
---
+++ BinaryStars.Domain/Errors/Devices/DeviceErrors.cs
@@ -0,0 +1,8 @@
+namespace BinaryStars.Domain.Errors.Devices;
+
+public static class DeviceErrors
+{
+    public const string IdCannotBeNullOrWhitespace = "Id cannot be null or whitespace";
+    public const string NameCannotBeNullOrWhitespace = "Name cannot be null or whitespace";
+    public const string IpAddressCannotBeNullOrWhitespace = "IpAddress cannot be null or whitespace";
+}
