2716849015cfd12cf4659698ec5a0dce0bc5f044
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2026-02-03 16:41:48 -0600

    content editor + improved note model on android

--- BinaryStars.Android/app/src/main/java/com/tds/binarystars/api/Models.kt
+++ BinaryStars.Android/app/src/main/java/com/tds/binarystars/api/Models.kt
@@ -23,10 +23,15 @@ data class AuthResponse(
     val expiresIn: Int
 )
 
+enum class DeviceTypeDto {
+    Linux,
+    Android
+}
+
 data class DeviceDto(
     val id: String,
     val name: String,
-    val type: Int, // Enum mapping needed
+    val type: DeviceTypeDto,
     val ipAddress: String,
     val batteryLevel: Int,
     val isOnline: Boolean,
