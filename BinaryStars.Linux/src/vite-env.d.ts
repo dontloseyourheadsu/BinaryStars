2ca39df7d6eb5cc0279895d97ff0d79dc05cb9d0
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2026-02-20 23:05:34 -0600

    OAuth validation on browser on tauri app

--- BinaryStars.Linux/src/vite-env.d.ts
+++ BinaryStars.Linux/src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+	readonly VITE_API_BASE_URL?: string;
+	readonly VITE_GOOGLE_CLIENT_ID?: string;
+	readonly VITE_GOOGLE_REDIRECT_URI?: string;
+	readonly VITE_MICROSOFT_CLIENT_ID?: string;
+	readonly VITE_MICROSOFT_TENANT_ID?: string;
+	readonly VITE_MICROSOFT_REDIRECT_URI?: string;
+	readonly VITE_MICROSOFT_SCOPE?: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
