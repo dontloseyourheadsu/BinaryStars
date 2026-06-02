c55a365f8288570f0fff13516ca4fca0531507a9
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2026-06-02 14:27:28 -0600

    chore: remove legacy C# .NET Avalonia codebase

--- BinaryStars/Models/NetworkPacket.cs
+++
@@ -1,8 +0,0 @@
-namespace BinaryStars.Models;
-
-public class NetworkPacket
-{
-    public string Type { get; set; } = ""; // "Handshake"
-    public string? SenderId { get; set; }
-    public string? SenderName { get; set; }
-}
--- BinaryStars/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace BinaryStars.ViewModels;
-
-public abstract class ViewModelBase : ObservableObject
-{
-}
