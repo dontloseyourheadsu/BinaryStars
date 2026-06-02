66f92fde55f694c29583c0c90bc41de1daf54639
Jesus Alvarez Sombrerero <dontloseyourheadsu@gmail.com>
2026-06-02 14:27:52 -0600

    feat(android): design main screen ui with safe notch padding and notion theme

---
+++ android/app/src/main/java/com/tds/binarystars/presentation/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.tds.binarystars.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Notion Dark Theme Colors
+val DarkBackground = Color(0xFF191919)
+val DarkCardBg = Color(0xFF202020)
+val DarkBorder = Color(0xFF2E2E2E)
+val PrimaryBlue = Color(0xFF2383E2)
+val OutgoingDarkBg = Color(0xFF2383E2)
+val IncomingDarkBg = Color(0xFF2C2C2C)
+val TextDarkPrimary = Color(0xFFFFFFFF)
+val TextDarkSecondary = Color(0xFF9B9B9B)
+
+// Notion Light Theme Colors
+val LightBackground = Color(0xFFFFFFFF)
+val LightCardBg = Color(0xFFF7F7F7)
+val LightBorder = Color(0xFFE9E9E9)
+val OutgoingLightBg = Color(0xFF2383E2)
+val IncomingLightBg = Color(0xFFF1F1F1)
+val TextLightPrimary = Color(0xFF37352F)
+val TextLightSecondary = Color(0xFF7C7B77)
